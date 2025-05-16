a78cc07bffe7e70862b4c3b276330e008e27d51d
Subin Gurung <grgsubn@gmail.com>
2025-05-16 22:28:50 +0545

    Fixed module-alias import

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import 'module-alias/register';
-import '@module-alias';
+import './module-alias';
 import app from './app';
 import dotenv from 'dotenv';
 import rateLimit from 'express-rate-limit';
